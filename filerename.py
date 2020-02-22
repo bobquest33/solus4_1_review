d23b1f7e9124acf387fbb6a61f84d86b81b4ef7c
Priyabrata Dash <bobquest33@gmail.com>
2020-02-23 01:29:09 +0530

    Updated Images

--- filerename.py
+++ filerename.py
@@ -1,8 +1,16 @@
 import os
+from PIL import Image
 
 for f in os.listdir():
-    if f.startswith('solus'):
+    if f.startswith('Solus'):
         
         nf = f.replace(" ",'-')
         print(f,nf)
         os.rename(f,nf)
+        try:
+            foo = Image.open(f)
+            print(foo.size)
+            foo = foo.resize((160,300),Image.ANTIALIAS)
+            foo.save(f,quality=95)
+        except Exception as e:
+            print(e)
