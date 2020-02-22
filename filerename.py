a33ec66e142adbd24d310fb06f3cd08e013598c9
Priyabrata Dash <bobquest33@gmail.com>
2020-02-23 01:43:18 +0530

    Reverting all changes

--- filerename.py
+++ filerename.py
@@ -1,5 +1,4 @@
 import os
-from PIL import Image
 
 for f in os.listdir():
     if f.startswith('Solus'):
@@ -7,12 +6,4 @@ for f in os.listdir():
         nf = f.replace(" ",'-')
         print(f,nf)
         os.rename(f,nf)
-        try:
-            foo = Image.open(f)
-            width,height = foo.size
-            nwidth = int(width*0.75)
-            nheight = int(height*0.75)
-            foo = foo.resize((nwidth,nheight),Image.ANTIALIAS)
-            foo.save(f,quality=95)
-        except Exception as e:
-            print(e)
+        
